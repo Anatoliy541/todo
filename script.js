b788c1d29ae09dfba989c0fd56d3ae0ffbebf641
Anatoliy541 <thinger703@gmail.com>
2025-11-29 23:06:40 +0300

    2 commit

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-   const todoInput = document.getElementById('todo-input');
-   const addTodoButton = document.getElementById('add-todo');
-   const todoList = document.getElementById('todo-list');
-
-   addTodoButton.addEventListener('click', () => {
-       const todoText = todoInput.value.trim();
-       if (todoText) {
-           const li = document.createElement('li');
-           li.textContent = todoText;
-           const deleteButton = document.createElement('button');
-           deleteButton.textContent = 'Удалить';
-           deleteButton.addEventListener('click', () => {
-               todoList.removeChild(li);
-           });
-           li.appendChild(deleteButton);
-           todoList.appendChild(li);
-           todoInput.value = '';
-       }
-   });
-});
+//document.querySelector('button').onclick = myClick;
+document.querySelector('button').addEventListener('click', myClick);
+function myClick() {
+    //console.log(work);
+    //get input
+    //input value
+    let a = document.querySelector('.i-1').value;
+    console.log(a);
+    document.querySelector('.out').innerHTML = a;
+}
